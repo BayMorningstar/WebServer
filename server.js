9baffe98cf719c200fae2a271b1344ba47636cab
Baymorningstar <halim.ceylan7@gmail.com>
2022-02-18 12:30:37 +0000

    added reCaptcha and footer changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 app = express();
 
 var https = require("https");
-app.use(express.static("localhost.com"));
+app.use(express.static("18.217.70.29"));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.get('/', function(req,res){
@@ -45,8 +45,6 @@ request.end();
 
 });
 const server = http.createServer(app);
-const port = 5000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
-
-
