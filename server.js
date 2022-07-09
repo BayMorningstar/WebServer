b54c93716bb62bbd960fd23d511a3b830ebe904f
Halim Ceylan <halim.ceylan7@gmail.com>
2022-07-09 22:35:16 +0300

    "octokit setup"

--- server.js
+++ server.js
@@ -1,53 +1,33 @@
 const http = require('http');
 var express = require('express');
 var fs = require('fs');
-const path = require('path');
+const path = require('path');   
 var MarkdownIt = require('markdown-it'),
     md = new MarkdownIt();
 app = express();
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env.AUTH,
+});
 var https = require("https");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.set('view engine', 'ejs');
 require('dotenv').config();
-function page(req,res){
-    var userName='BayMorningstar';
-    var options = {
-      host :"api.github.com",
-      path: "/users/" +userName+ "/repos",
-      method : 'GET',
-      headers: {'User-Agent':'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'},
-      auth: process.env.AUTH || "",
-    }
-    var request = https.request(options, function(response){
-      var body = '';
-      response.on('data',function(chunk){
-          body+=chunk;
-      });
-      response.on('end',function(){
-          var json = JSON.parse(body);
-          var repos =[];
-          json.forEach(function(repo){
-              repos.push({
-                  name : repo.name,
-                  description : repo.description,
-                  date : repo.created_at,
-                  url : repo.html_url,
-                  lang : repo.language
-              });
-          });
-          var path = __dirname + '/public/markdown/resume.md';
-          var file = fs.readFileSync(path, 'utf8');
-          var result = md.render(file);
-          res.render(__dirname + "/public/index", {my_repos : JSON.stringify(repos),resume :result});
-      });
-  });
-  request.on('error', function(e) {
-      console.error('and the error is '+e);
-  });
-  request.end();
-  }
-app.get('/',page);
+function reposData(req,res){
+    octokit
+    .paginate("GET /users/{owner}/repos", {
+        owner: "HalimACeylan",
+      })
+      .then((repos) => {
+        // issues is an array of all issue objects. It is not wrapped in a { data, headers, status, url } object
+        // like results from `octokit.request()` or any of the endpoint methods such as `octokit.rest.issues.listForRepo()`
+        var path = __dirname + '/public/markdown/resume.md';
+        var file = fs.readFileSync(path, 'utf8');
+        var result = md.render(file);
+        res.render(__dirname + "/public/index", {my_repos : JSON.stringify(repos),resume :result});
+      })}
+app.get('/',reposData);
 const server = http.createServer(app);
 const port = process.env.PORT || 80;
 server.listen(port);
